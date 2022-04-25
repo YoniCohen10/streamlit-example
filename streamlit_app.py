7669395aebde2f9345129cbdd876a26d67cb8a70
ycohen <yonicohen10@gmail.com>
2022-04-25 12:17:32 +0300

    change header 3

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,10 +31,10 @@ st.set_page_config(page_icon="🧠", page_title="DSandbox")
 # st.image("https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/balloon_1f388.png", width=100)
 st.image(
     "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/325/brain_1f9e0.png",
-    width=100,
+    width=500,
 )
 
-st.title("DSandbox")
+st.title("                                                 DSandbox")
 
 # st.caption(
 #     "PRD : TBC | Streamlit Ag-Grid from Pablo Fonseca: https://pypi.org/project/streamlit-aggrid/"
