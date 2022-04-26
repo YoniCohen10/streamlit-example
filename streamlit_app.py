f935549759b540f08647c0827bca0729ede41aee
ycohen <yonicohen10@gmail.com>
2022-04-26 16:18:29 +0300

    add train progress bar

--- streamlit_app.py
+++ streamlit_app.py
@@ -298,8 +298,8 @@ if st.button('Train model!') and legit and col_to_drop.count(target_feature) < 1
 
 if train_over:
     if ModelType == 'Classification (Default)':
+        class_threshold = st.slider("enter classification threshold:", min_value=0.01, max_value=0.99, value=0.5)
         while True:
-            class_threshold = st.slider("enter classification threshold:", min_value=0.01, max_value=0.99, value=0.5)
             if class_threshold is not None and st.button('Update results'):
                 pereds_label = np.where(pereds > class_threshold, 1, 0)
 
