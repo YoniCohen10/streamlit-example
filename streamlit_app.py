93f18035e9c3531dc66c600596138f66ba214f46
ycohen <yonicohen10@gmail.com>
2022-04-26 13:52:07 +0300

    add train progress bar

--- streamlit_app.py
+++ streamlit_app.py
@@ -299,7 +299,7 @@ if train_over:
     if ModelType == 'Classification (Default)':
         class_threshold = ''
         while class_threshold == '':
-            class_threshold = st.text_input("enter classification threshold:", placeholder='0.5')
+            class_threshold = st.text_input("enter classification threshold:", placeholder='0.5', key=123)
         try:
             float(class_threshold)
             if class_threshold < 0.00001 or class_threshold > 0.9999999:
