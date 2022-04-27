09ad40ce7057e88f1351b6d78036ae4fdba7c045
ycohen <yonicohen10@gmail.com>
2022-04-27 18:35:08 +0300

    add train progress bar

--- streamlit_app.py
+++ streamlit_app.py
@@ -302,8 +302,8 @@ if not legit or not (col_to_drop.count(target_feature) < 1):
 
 train_over = False
 if st.button('Train model!') and legit and col_to_drop.count(target_feature) < 1:
-    st.success(f""" 🏃  Everything looks great! Start Training!""")
     try:
+        st.success(f""" 🏃  Everything looks great! Start Training!""")
         with st.spinner('Wait for it...'):
             bst, pereds, X_train, X_test, y_train, y_test, param = train_model(shows, ModelType, target_feature,
                                                                                random_or_date,
