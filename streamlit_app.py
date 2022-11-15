8f9bdc2931a5e9bd9c09b47babf68ec1a63e42fb
ycohen <yonicohen10@gmail.com>
2022-11-15 15:28:15 +0200

    plotly check

--- streamlit_app.py
+++ streamlit_app.py
@@ -465,8 +465,7 @@ if col4.button('Train model!') and st.session_state.legit and col_to_drop.count(
 
         middle = int((max(pereds) - min(pereds)) / 10)
         fig = ff.create_distplot([np.transpose(pereds)], ['Regressor predictions'], show_curve=False, colors=['red'],
-                                 histnorm='',
-                                 bin_size=list(np.linspace(max(pereds), min(pereds))))
+                                 histnorm='')
 
         # Plot!
         st.plotly_chart(fig, use_container_width=True)
